8ee2c607a325d40a4fc6435c84ce86994925ea22
Oda <oda.schneider@gmx.de>
2022-11-10 22:30:18 +0100

    added profile picture

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AuthService } from '../services/auth.service';
+import { User } from '../services/user';
 
 @Component({
   selector: 'app-mainpage',
@@ -8,9 +10,33 @@ import { AuthService } from '../services/auth.service';
 })
 export class MainpageComponent implements OnInit {
 
-  constructor(public authService: AuthService) {}
+  users = [];
+  currentUserId = '';
+  currentUser = [];
+
+  constructor(public authService: AuthService, private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.firestore
+      .collection('users')
+      .valueChanges({ idField: 'userId' })
+      .subscribe((users: any) => {
+        this.users = users;
+        this.getCurrentUserId();
+      })
   }
 
+
+  getCurrentUserId(){
+      let currentUser = localStorage.getItem('user');
+      currentUser = JSON.parse(currentUser);
+      this.currentUserId = currentUser['uid'];
+      this.setCurrentUser();
+  }
+
+
+  setCurrentUser(){
+    let currentUser = this.users.filter((user:any) => user['uid'] == this.currentUserId);
+    this.currentUser = currentUser[0];
+  }
 }
