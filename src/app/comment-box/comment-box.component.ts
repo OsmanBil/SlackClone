98db8c4cc13cf8d168a5b7db30c0bb17beeb6b12
Oda <oda.schneider@gmx.de>
2022-11-23 20:20:04 +0100

    add and dislpay post in channel

--- src/app/comment-box/comment-box.component.ts
+++ src/app/comment-box/comment-box.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { EditorChangeContent, EditorChangeSelection, QuillEditorComponent } from 'ngx-quill';
+import { getFirestore } from '@firebase/firestore';
+import { collection, addDoc, getDocs, doc, orderBy, Timestamp, setDoc, serverTimestamp, updateDoc, getDoc, onSnapshot, query, where } from "firebase/firestore";
 
 @Component({
   selector: 'app-comment-box',
@@ -12,10 +15,12 @@ import { EditorChangeContent, EditorChangeSelection, QuillEditorComponent } from
 export class CommentBoxComponent implements OnInit {
 
   form: FormGroup;
+  db = getFirestore();
 
   @ViewChild('editor', {
     static: true
   }) editor: QuillEditorComponent
+
 
   modules = {
     toolbar: [
@@ -24,13 +29,23 @@ export class CommentBoxComponent implements OnInit {
       [{ 'list': 'ordered' }, { 'list': 'bullet' }],
       ['link'],
       ['image'],
-      ['emoji']
     ],
+  }
+
+  data = {
+    text: '',
+    time: Timestamp.fromDate(new Date()),
+    author: '',
+    userId: '',
+    img: ''
   }
 
   text: string;
 
-  constructor() {
+  channelId: string;
+  currentUser = [];
+
+  constructor(private route: ActivatedRoute) {
     this.form = new FormGroup({
       'editor': new FormControl()
     });
@@ -42,11 +57,32 @@ export class CommentBoxComponent implements OnInit {
 
 
   changedEditor(event: EditorChangeContent | EditorChangeSelection) {
-    // tslint:disable-next-line:no-console
     if(event['event']== 'text-change'){
       this.text = event['html'];
-      console.log(this.text);
     }
   }
 
+
+  postToChannel(){
+    this.currentUser = JSON.parse(localStorage.getItem('user'));
+    this.route.paramMap.subscribe(paramMap => {
+      this.channelId = paramMap.get('id');
+    })
+    this.setData();
+  }
+
+
+  setData(){
+    this.data.author = this.currentUser['displayName'];
+    this.data.userId = this.currentUser['uid'];
+    this.data.img = this.currentUser['photoURL'];
+    this.data.text = this.text;
+    this.data.time = Timestamp.fromDate(new Date());
+    this.sendData();
+  }
+
+
+  async sendData(){
+    await addDoc(collection(this.db, "channels", this.channelId, "posts"), this.data);
+  }
 }
--- src/app/open-channel/open-channel.component.ts
+++ src/app/open-channel/open-channel.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { Channel } from 'src/models/channel.class';
+import { getFirestore } from '@firebase/firestore';
+import { collection, addDoc, getDocs, doc, orderBy, Timestamp, setDoc, serverTimestamp, updateDoc, getDoc, onSnapshot, query, where } from "firebase/firestore";
 
 @Component({
   selector: 'app-open-channel',
@@ -7,9 +12,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OpenChannelComponent implements OnInit {
 
-  constructor() { }
+  db = getFirestore();
+  channelId = '';
+  channel: Channel = new Channel();
+  posts = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private firestore: AngularFirestore,
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(paramMap => {
+      this.channelId = paramMap.get('id');
+      this.firestore
+      .collection('channels')
+      .doc(this.channelId)
+      .valueChanges()
+      .subscribe((channel: any) => {
+        this.channel = new Channel(channel);
+        this.loadMessages();
+      })
+    })
+  }
+
+
+  loadMessages() {
+    const ref = collection(this.db, "channels", this.channelId, "posts");
+    const q = query(ref, orderBy("time"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      this.posts = [];
+      snapshot.forEach((doc) => {
+        let post = { 
+          text: doc.data()['text'], 
+          time: this.convertTimestamp(doc.data()['time']), 
+          author: doc.data()['author'], 
+          img: doc.data()['img'],
+          id: doc.id };
+        this.posts.push(post);
+      });
+    });
+  }
+
+
+  convertTimestamp(timestamp) {
+    let date = timestamp.toDate();
+    let mm = date.getMonth();
+    let dd = date.getDate();
+    let yyyy = date.getFullYear();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let secondes = date.getSeconds();
+    if (secondes.lenght < 10) {
+      secondes = '0' + secondes
+    }
+    if (hours.lenght < 10) {
+      hours = '0' + hours
+    }
+    if (minutes < 10) {
+      minutes = '0' + minutes
+    }
+    date = dd + '/' + (mm + 1) + '/' + yyyy + ' ' + hours + ':' + minutes;
+    return date;
   }
 
 }
