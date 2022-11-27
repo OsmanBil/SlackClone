36e7b6c3aee29ee2c15eebaae65729c948ee0a6b
FabianKalus <104758870+FabianKalus@users.noreply.github.com>
2022-11-27 14:16:42 +0100

    clean code sidebar

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -69,76 +69,52 @@ export class SidenavComponent implements OnInit {
     }
   }
 
+  async loadChatIDs() {
+    this.chatUserIDs = [];
+    for (let x = 0; x < this.chatIDs.length; x++) {
+      let docRef2 = collection(this.db, "chatrooms", this.chatIDs[x], "users");
+      let querySnapshot2 = await getDocs(docRef2);
+      querySnapshot2.forEach(async (doc2: any) => {
+        if (doc2.data().id !== this.localUser['uid']) {
+          this.chatUserIDs.push({ userID: doc2.data().id, name: doc2.data().name, chatID: this.chatIDs[x] })
+          console.log(this.chatUserIDs)
+        }
+      })
+    }
+  };
+
+  async loadChatroomUserData(){
+    this.chatrooms = [];
+      for (let i = 0; i < this.chatUserIDs.length; i++) {
+        const unsub = onSnapshot(doc(this.db, "users", this.chatUserIDs[i].userID), { includeMetadataChanges: true },
+          (doc: any) => {
+            let chatrommUser = {
+              userID: doc.data().id, chatroomID: this.chatUserIDs[i].chatID, name: doc.data().displayName,
+              isOnline: doc.data().isOnline, photoURL: doc.data().photoURL, localIndex: i
+            }
+            if (this.chatrooms[chatrommUser.localIndex]) {
+              this.chatrooms[chatrommUser.localIndex] = chatrommUser
+            }
+            else {
+              this.chatrooms.push(chatrommUser)
+            }
+          })
+      }
+  }
+
   async loadChatrooms() {
     this.localUser = JSON.parse(localStorage.getItem('user'))
     // LOAD USER FROM CHATROOM
     let docRef = collection(this.db, "users", this.localUser['uid'], "chatids");
-
-    // let querySnapshot = await getDocs(docRef);
     const unsubscribe = onSnapshot(docRef, async (querySnapshot) => {
       this.chatIDs = [];
       querySnapshot.forEach(async (doc) => {
-        // let docRef2 = collection(this.db, "chatrooms", doc.id, "users");
-        // let querySnapshot2 = await getDocs(docRef2);
         this.chatIDs.push(doc.id)
         console.log(this.chatIDs)
       })
-      this.chatUserIDs = [];
-      for (let x = 0; x < this.chatIDs.length; x++) {
-        let docRef2 = collection(this.db, "chatrooms", this.chatIDs[x], "users");
-        let querySnapshot2 = await getDocs(docRef2);
-        querySnapshot2.forEach(async (doc2: any) => {
-          if (doc2.data().id !== this.localUser['uid']) {
-            // currentChatroom.id = doc2.data().id;
-            // this.chatrooms.push({ name: doc2.data().name, photoURL: doc2.data().photoURL, id: doc.id, shownInSidebar: '' })
-            this.chatUserIDs.push({ userID: doc2.data().id, name: doc2.data().name, chatID: this.chatIDs[x] })
-            console.log(this.chatUserIDs)
-          }
-        })
-
-      }
-
-      this.chatrooms = [];
-      for(let i = 0; i < this.chatUserIDs.length; i++){
-        const unsub = onSnapshot(doc(this.db, "users", this.chatUserIDs[i].userID), {includeMetadataChanges: true}, 
-        (doc: any) => {
-           let chatrommUser = { userID: doc.data().id, chatroomID: this.chatUserIDs[i].chatID,  name: doc.data().displayName,
-             isOnline: doc.data().isOnline, photoURL: doc.data().photoURL, localIndex: i}
-            if(this.chatrooms[chatrommUser.localIndex]) {
-              this.chatrooms[chatrommUser.localIndex] = chatrommUser
-            }
-            else {
-           this.chatrooms.push(chatrommUser)
-            }
-        })
-      }
-
-
-
-      // this.chatrooms = [];
-      // const unsubscribe2 = onSnapshot(docRef2, (querySnapshot2) => {
-      // querySnapshot2.forEach(async (doc2: any) => {
-      //   if (doc2.data().id !== this.localUser['uid']) {
-      //     // currentChatroom.id = doc2.data().id;
-      //     // this.chatrooms.push({ name: doc2.data().name, photoURL: doc2.data().photoURL, id: doc.id, shownInSidebar: '' })
-      //     this.chatrooms.push({ userID: doc2.data().id, chatroomID: doc.id,  name: doc2.data().name,})
-      //   }
-
-      // })
-      // })
-
-
-      // querySnapshot2.forEach((doc2: any) => {
-      //   if (doc2.data().name == this.localUser['displayName']) {
-      //     currentChatroom.shownInSidebar = doc2.data().shownInSidebar;
-      //   }
-      //   else {
-      //     currentChatroom.name = doc2.data().name;
-      //     currentChatroom.photoURL = doc2.data().photoURL;
-      //     currentChatroom.id = doc.id;
-      //     this.chatrooms.push({ name: currentChatroom.name, photoURL: currentChatroom.photoURL, id: doc.id })
-      //   }
-      //})
+      await this.loadChatIDs();
+      await this.loadChatroomUserData();
+      
     })
     this.firestore
       .collection('channels')
