b6f827ccc04118c26cec3d57854a12c2bf15550b
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2022-12-08 05:01:50 +0100

    threads test

--- src/app/threads/threads.component.ts
+++ src/app/threads/threads.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -12,6 +12,8 @@ import * as firebase from "firebase/app";
 import 'firebase/firestore';
 import { Channel } from 'src/models/channel.class';
 import { fromEvent, timestamp } from 'rxjs';
+import { LightboxComponent } from '../lightbox/lightbox.component';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-threads',
@@ -19,117 +21,103 @@ import { fromEvent, timestamp } from 'rxjs';
   styleUrls: ['./threads.component.scss']
 })
 export class ThreadsComponent implements OnInit {
-  testVar: any = "";
-  db = getFirestore();
-  channelId = '';
-  channel: Channel = new Channel();
-  posts = [];
-  varTest = 'Hallo'
+
+
+  channelId = 'Ay2zQKNVkwbRUDWOyqXC';
+  postId = 'iPCGASHA62U7wMwI4D8J'
+
+  threadName = '';
+  channelName = '';
+  
+
   allThreads = [];
-  localUser;
-  counter = 0;
-  userId = '';
+
+
+
+  db = getFirestore();
+  comments = [];
+
 
   constructor(private route: ActivatedRoute, public dialog: MatDialog, private firestore: AngularFirestore, private router: Router, public fr: Firestore) { }
 
   async ngOnInit(): Promise<void> {
-    await this.loadUser()
-    await this.loadThreads();
+  await  this.loadAll()
+
   }
 
-  async loadThreads() {
-    let ref = collection(this.db, "users", this.userId, "threads");
-    let y = query(ref, orderBy("time", "desc"));
-    let unsubscribe = await onSnapshot(y, (snapshot) => {
-      this.allThreads = [];
-      this.counter = -1;
-      snapshot.forEach((doc) => {
-        this.counter++;
-        this.channelId = doc.id;
-        this.loadFirstMessage(this.counter);
-        this.loadMessages(this.counter);
-        this.loadChannelNames(this.counter)
-        this.allThreads.push({ id: this.channelId, content: [] });
-      });
-      unsubscribe()
+  async loadAll(){
+   await this.loadComments();
+    this.loadThread()
+    
+
+    
+  }
+
+  loadThread() {
+    this.firestore.collection(`channels`).doc(this.channelId).collection('posts').doc(this.postId).get().subscribe(async ref => {
+      const doc: any = ref.data();
+      // console.log('Thread:', doc)
+      this.threadName = doc.text;
+
+      //this.allThreads.push({id: this.channelId, threadName: doc['text']})
+
     });
+
+    // this.firestore.collection(`channels`).doc(this.channelId).collection('posts').doc(this.postId).collection('comments').doc('gImJhZUpxI8yBUEJHR3N').get().subscribe(async ref => {
+    //   const doc: any = ref.data();
+    //   console.log('Thread:', doc)
+    // });
+    this.loadChannelNames()
   }
 
-  loadUser() {
-    // Get the existing data
-    var existing = localStorage.getItem('user');
-    // If no existing data, create an array
-    // Otherwise, convert the localStorage string to an array
-    existing = existing ? JSON.parse(existing) : {};
-    this.userId = existing['uid'];
-    // console.log('USER: ', existing['uid'])
-  }
-
-  loadChannelNames(counter) {
+  loadChannelNames() {
+   
     this.firestore.collection(`channels`).doc(this.channelId).get().subscribe(async ref => {
-      // const doc: any = ref.data();
-      this.testVar = ref.data()
-      this.allThreads[counter].channelName = this.testVar.channelName;
-      // this.testArray.push({ channelName: this.testVar.channelName })
+       const doc: any = ref.data();
+     // console.log('Channel Name: #',  doc.channelName)
+      this.channelName = doc.channelName
+      this.allThreads.push({id: this.channelId, threadName: this.threadName, channelName: doc.channelName, comments : this.comments})
+ 
     });
+
+    console.log('allThreads:', this.allThreads)
   }
 
-  // loadMessages(counter) {
-  //     const visitArray = this.firestore.collection("channels").doc(this.channelId).collection("posts").snapshotChanges();
-  //    visitArray.subscribe(payload => {
-  //        this.totalVisitCount = payload.length;
-  //        console.log('length:', this.totalVisitCount)
-  //     if (this.totalVisitCount > 1) {
-  //       let ref = collection(this.db, "channels", this.channelId, "posts");
-  //       let q = query(ref, orderBy("time", "asc"), limitToLast(2));
-  //       let unsubscribe = onSnapshot(q, (snapshot) => {
-  //         this.allThreads[counter].content = []
-  //         snapshot.forEach((postDoc) => {
-  //           this.loadAuthor(postDoc, counter);
-  //         });
-  //         unsubscribe()
-  //       });
-  //     }
-  //   });
-  // }
 
-  async loadMessages(counter) {
-    let ref = collection(this.db, "channels", this.channelId, "posts");
-    let q = query(ref, orderBy("time", "asc"), limitToLast(2));
+
+  async loadComments() {
+    let commentRef = collection(this.db, "channels", this.channelId, "posts", this.postId, 'comments');
+    
+    let q = query(commentRef, orderBy("time"));
     let unsubscribe = onSnapshot(q, async (snapshot) => {
-      this.allThreads[counter].content = []
-      await snapshot.forEach((postDoc) => {
-        this.loadAuthor(postDoc, counter);
+      this.comments = [];
+      snapshot.forEach((commentDoc) => {
+        let comment = {
+          text: commentDoc.data()['text'],
+          time: this.convertTimestamp(commentDoc.data()['time']),
+          author: '',
+          img: '',
+          commentId: commentDoc.id,
+          userId: commentDoc.data()['userId'],
+          uploadImg: commentDoc.data()['upload']
+        };
+        this.loadAuthor(comment);
       });
-      unsubscribe()
     });
   }
 
-  async loadFirstMessage(counter) {
-    let ref = collection(this.db, "channels", this.channelId, "posts");
-    let y = query(ref, orderBy("time"), limit(1));
-    let unsubscribe = await onSnapshot(y, (snapshot) => {
-      this.allThreads[counter].content = []
-      snapshot.forEach((postDoc) => {
-        this.loadAuthor(postDoc, counter);
+  async loadAuthor(comment) {
+    let user = query(collection(this.db, "users"), where("uid", "==", comment.userId));
+    let unsubscribe = onSnapshot(user, async (snapshot) => {
+      snapshot.forEach((userDoc) => {
+        comment.author = userDoc.data()['displayName'];
+        comment.img = userDoc.data()['photoURL'];
       });
-      unsubscribe()
     });
-  }
+    this.comments.push(comment);
 
-  async loadAuthor(postDoc, counter) {
-    let user = query(collection(this.db, "users"), where("uid", "==", postDoc.data()['userId']));
-    let querySnapshot = await getDocs(user);
-    querySnapshot.forEach((userDoc) => {
-      let post = {
-        text: postDoc.data()['text'],
-        time: this.convertTimestamp(postDoc.data()['time']),
-        author: userDoc.data()['displayName'],
-        img: userDoc.data()['photoURL'],
-        id: postDoc.id
-      };
-      this.allThreads[counter].content.push(post);
-    });
+    console.log('Comments:', this.comments)
+    unsubscribe()
   }
 
 
@@ -153,5 +141,172 @@ export class ThreadsComponent implements OnInit {
     date = dd + '/' + (mm + 1) + '/' + yyyy + ' ' + hours + ':' + minutes;
     return date;
   }
+
+
+
+  openLightbox(url){
+    let dialog = this.dialog.open(LightboxComponent);
+    dialog.componentInstance.lightboxImg = url;
+  }
+
+  // import { Component, OnInit } from '@angular/core';
+  // import { AngularFirestore } from '@angular/fire/compat/firestore';
+  // import { MatDialog } from '@angular/material/dialog';
+  // import { ActivatedRoute, Router } from '@angular/router';
+  // import { DialogCreateChannelComponent } from '../dialog-create-channel/dialog-create-channel.component';
+  // import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+  // import { AngularFireDatabase } from '@angular/fire/compat/database';
+  // import { getFirestore } from '@firebase/firestore';
+  // import { Firestore, CollectionReference, collectionData, docData, collection, addDoc, getDocs, doc, orderBy, Timestamp, setDoc, serverTimestamp, updateDoc, getDoc, onSnapshot, query, where, limit, limitToLast, getCountFromServer } from '@angular/fire/firestore';
+  // import { user } from '@angular/fire/auth';
+  // import * as firebase from "firebase/app";
+  // import 'firebase/firestore';
+  // import { Channel } from 'src/models/channel.class';
+  // import { fromEvent, timestamp } from 'rxjs';
+  
+  // @Component({
+  //   selector: 'app-threads',
+  //   templateUrl: './threads.component.html',
+  //   styleUrls: ['./threads.component.scss']
+  // })
+  // export class ThreadsComponent implements OnInit {
+  //   testVar: any = "";
+  //   db = getFirestore();
+  //   channelId = '';
+  //   channel: Channel = new Channel();
+  //   posts = [];
+  //   varTest = 'Hallo'
+  //   allThreads = [];
+  //   localUser;
+  //   counter = 0;
+  //   userId = '';
+  
+  //   constructor(private route: ActivatedRoute, public dialog: MatDialog, private firestore: AngularFirestore, private router: Router, public fr: Firestore) { }
+  
+  //   async ngOnInit(): Promise<void> {
+  //     await this.loadUser()
+  //     await this.loadThreads();
+  //   }
+  
+  //   async loadThreads() {
+  //     let ref = collection(this.db, "users", this.userId, "threads");
+  //     let y = query(ref, orderBy("time", "desc"));
+  //     let unsubscribe = await onSnapshot(y, (snapshot) => {
+  //       this.allThreads = [];
+  //       this.counter = -1;
+  //       snapshot.forEach((doc) => {
+  //         this.counter++;
+  //         this.channelId = doc.id;
+  //         this.loadFirstMessage(this.counter);
+  //         this.loadMessages(this.counter);
+  //         this.loadChannelNames(this.counter)
+  //         this.allThreads.push({ id: this.channelId, content: [] });
+  //       });
+  //       unsubscribe()
+  //     });
+  //   }
+  
+  //   loadUser() {
+  //     // Get the existing data
+  //     var existing = localStorage.getItem('user');
+  //     // If no existing data, create an array
+  //     // Otherwise, convert the localStorage string to an array
+  //     existing = existing ? JSON.parse(existing) : {};
+  //     this.userId = existing['uid'];
+  //     // console.log('USER: ', existing['uid'])
+  //   }
+  
+  //   loadChannelNames(counter) {
+  //     this.firestore.collection(`channels`).doc(this.channelId).get().subscribe(async ref => {
+  //       // const doc: any = ref.data();
+  //       this.testVar = ref.data()
+  //       this.allThreads[counter].channelName = this.testVar.channelName;
+  //       // this.testArray.push({ channelName: this.testVar.channelName })
+  //     });
+  //   }
+  
+  //   // loadMessages(counter) {
+  //   //     const visitArray = this.firestore.collection("channels").doc(this.channelId).collection("posts").snapshotChanges();
+  //   //    visitArray.subscribe(payload => {
+  //   //        this.totalVisitCount = payload.length;
+  //   //        console.log('length:', this.totalVisitCount)
+  //   //     if (this.totalVisitCount > 1) {
+  //   //       let ref = collection(this.db, "channels", this.channelId, "posts");
+  //   //       let q = query(ref, orderBy("time", "asc"), limitToLast(2));
+  //   //       let unsubscribe = onSnapshot(q, (snapshot) => {
+  //   //         this.allThreads[counter].content = []
+  //   //         snapshot.forEach((postDoc) => {
+  //   //           this.loadAuthor(postDoc, counter);
+  //   //         });
+  //   //         unsubscribe()
+  //   //       });
+  //   //     }
+  //   //   });
+  //   // }
+  
+  //   async loadMessages(counter) {
+  //     let ref = collection(this.db, "channels", this.channelId, "posts");
+  //     let q = query(ref, orderBy("time", "asc"), limitToLast(2));
+  //     let unsubscribe = onSnapshot(q, async (snapshot) => {
+  //       this.allThreads[counter].content = []
+  //       await snapshot.forEach((postDoc) => {
+  //         this.loadAuthor(postDoc, counter);
+  //       });
+  //       unsubscribe()
+  //     });
+  //   }
+  
+  //   async loadFirstMessage(counter) {
+  //     let ref = collection(this.db, "channels", this.channelId, "posts");
+  //     let y = query(ref, orderBy("time"), limit(1));
+  //     let unsubscribe = await onSnapshot(y, (snapshot) => {
+  //       this.allThreads[counter].content = []
+  //       snapshot.forEach((postDoc) => {
+  //         this.loadAuthor(postDoc, counter);
+  //       });
+  //       unsubscribe()
+  //     });
+  //   }
+  
+  //   async loadAuthor(postDoc, counter) {
+  //     let user = query(collection(this.db, "users"), where("uid", "==", postDoc.data()['userId']));
+  //     let querySnapshot = await getDocs(user);
+  //     querySnapshot.forEach((userDoc) => {
+  //       let post = {
+  //         text: postDoc.data()['text'],
+  //         time: this.convertTimestamp(postDoc.data()['time']),
+  //         author: userDoc.data()['displayName'],
+  //         img: userDoc.data()['photoURL'],
+  //         id: postDoc.id
+  //       };
+  //       this.allThreads[counter].content.push(post);
+  //     });
+  //   }
+  
+  
+  //   convertTimestamp(timestamp) {
+  //     let date = timestamp.toDate();
+  //     let mm = date.getMonth();
+  //     let dd = date.getDate();
+  //     let yyyy = date.getFullYear();
+  //     let hours = date.getHours();
+  //     let minutes = date.getMinutes();
+  //     let secondes = date.getSeconds();
+  //     if (secondes < 10) {
+  //       secondes = '0' + secondes
+  //     }
+  //     if (hours < 10) {
+  //       hours = '0' + hours
+  //     }
+  //     if (minutes < 10) {
+  //       minutes = '0' + minutes
+  //     }
+  //     date = dd + '/' + (mm + 1) + '/' + yyyy + ' ' + hours + ':' + minutes;
+  //     return date;
+  //   }
+  
+  // }
+  
+
 
 }
